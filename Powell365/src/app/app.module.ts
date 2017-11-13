14c844599f0f025e46004106a715d6305d1bd628
Ludovic Horem <ludo.horem@gmail.com>
2017-11-13 23:40:39 +0100

    import qrscanner + storage modules

--- Powell365/src/app/app.module.ts
+++ Powell365/src/app/app.module.ts
@@ -11,6 +11,9 @@ import { TabsPage } from '../pages/tabs/tabs';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+import { IonicStorageModule } from '@ionic/storage';
+import { QRScanner } from '@ionic-native/qr-scanner';
+
 @NgModule({
   declarations: [
     MyApp,
@@ -21,7 +24,9 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    QRScanner
   ],
   bootstrap: [IonicApp],
   entryComponents: [
